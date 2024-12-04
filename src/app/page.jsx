fc87a6b1a6702a69f562b8f75d886319bd32e710
Dimi <diba0001@stud.kea.dk>
2024-12-04 11:07:59 +0100

    Added: null component to test with

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-
+import IndexMotion from "@/components/IndexMotion"
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="">
 
       <Hero alt="Photo by Samu Lopez, from Unsplash FREE" />
-
+<IndexMotion />
     </div>
   );
 }
---
+++ src/components/IndexMotion.jsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react';
+
+const IndexMotion = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default IndexMotion;
